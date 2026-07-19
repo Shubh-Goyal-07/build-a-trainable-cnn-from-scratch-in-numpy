c90d0e9c27d8149a506718d4bf519f8aca9e5368
Shubh Goyal <goyal.shubh0711@gmail.com>
2026-07-19 15:57:24 +0530

    Step 53: shuffle_indices

--- model.py
+++ model.py
@@ -610,8 +610,12 @@ def build_synthetic_image_dataset(num_samples, num_classes, image_size, in_chann
 
     return x, y
 
-# Step 53 - shuffle_indices (not yet solved)
-# TODO: implement
+# Step 53 - shuffle_indices
+import numpy as np
+
+def shuffle_indices(n, seed=0):
+    rng = np.random.RandomState(seed)
+    return rng.permutation(n)
 
 # Step 54 - train_test_split (not yet solved)
 # TODO: implement
