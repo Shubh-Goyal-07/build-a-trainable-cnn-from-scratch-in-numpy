bc91c519a76381683ff81ef4d56e39b00675f712
Shubh Goyal <goyal.shubh0711@gmail.com>
2026-07-19 15:40:47 +0530

    Step 47: lenet_forward

--- model.py
+++ model.py
@@ -470,8 +470,39 @@ def forward_classifier_block(x, fc1, fc2):
 
     return x2, cache
 
-# Step 47 - lenet_forward (not yet solved)
-# TODO: implement
+# Step 47 - lenet_forward
+def lenet_forward(x, params):
+    out, block1_cache = forward_conv_block(
+        x,
+        params["conv1"]["W"],
+        params["conv1"]["b"],
+        pool_size=2,
+        stride=1,
+        pad=0
+    )
+
+    out, block2_cache = forward_conv_block(
+        out,
+        params["conv2"]["W"],
+        params["conv2"]["b"],
+        pool_size=2,
+        stride=1,
+        pad=0
+    )
+
+    logits, classifier_cache = forward_classifier_block(
+        out,
+        params["fc1"],
+        params["fc2"]
+    )
+
+    caches = {
+        "block1": block1_cache,
+        "block2": block2_cache,
+        "classifier": classifier_cache
+    }
+
+    return logits, caches
 
 # Step 48 - backward_conv_block (not yet solved)
 # TODO: implement
